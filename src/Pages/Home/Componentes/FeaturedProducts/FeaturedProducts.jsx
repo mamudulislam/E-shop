f8a2249eac8ddf86ab6e76d8a2f8d1c2ce02cc07
Mamudul_islam <mamuduli153@gmail.com>
2025-05-23 20:50:41 +0600

    salider buttun add

--- src/Pages/Home/Componentes/FeaturedProducts/FeaturedProducts.jsx
+++ src/Pages/Home/Componentes/FeaturedProducts/FeaturedProducts.jsx
@@ -1,13 +1,79 @@
-import React from 'react'
-import Container from '../../../../Golobalcomponentes/Container'
+import React from 'react';
+import Container from '../../../../Golobalcomponentes/Container';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { CgArrowLongRight } from 'react-icons/cg';
 import ProductCard from '../../../../Golobalcomponentes/ProductCard/ProductCard';
-import Productimg from '../../../../assets/Productimg/Productimg.webp'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { Featureddata } from './Featureddata';
+
+// Custom Arrow Components
+function NextArrow(props) {
+    const { onClick } = props;
+    return (
+        <div
+            className="absolute -right-6 top-1/2 -translate-y-1/2 z-10 p-2 bg-white border border-gray-300 rounded-full cursor-pointer shadow hover:bg-orange transition-colors"
+            onClick={onClick}
+        >
+            <CgArrowLongRight size={24} />
+        </div>
+    );
+}
+
+function PrevArrow(props) {
+    const { onClick } = props;
+    return (
+        <div
+            className="absolute -left-6 top-1/2 -translate-y-1/2 z-10 p-2 bg-white border border-gray-300 rounded-full cursor-pointer shadow hover:bg-orange transition-colors"
+            onClick={onClick}
+        >
+            <CgArrowLongRight size={24} className="rotate-180" />
+        </div>
+    );
+}
+
 const FeaturedProducts = () => {
     const { t } = useTranslation();
+
+    // Slider settings that match your grid layout
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    };
+
     return (
         <>
             <Container>
@@ -18,27 +84,34 @@ const FeaturedProducts = () => {
                         </h3>
                         <Link to="/Product" className='flex items-center gap-x-2 font-Montserrat font-bold text-base text-orange group'>
                             {t("View All")}{" "}
-                            <span className='group-hover:translate-x-2 transition-transform duration-400'>  <CgArrowLongRight size={30} /></span>
+                            <span className='group-hover:translate-x-2 transition-transform duration-400'>
+                                <CgArrowLongRight size={30} />
+                            </span>
                         </Link>
                     </div>
-                    <div className='grid grid-cols-5 gap-x-5 '>{
-                        Featureddata?.map((product) => (
-                            <ProductCard key={product.id}
-                                image={product?.imges}
-                                pCatagori={product?.pCatagori}
-                                pName={product?.pName}
-                                rate={product?.rate}
-                                totalrating={product?.totalrating}
-                                price={product?.price}
-                                pdiscout={product?.discout}
-                            />
-                        ))
-                    }</div>
 
+                    {/* Slider with custom arrows */}
+                    <div className='slider-container relative'>
+                        <Slider {...settings}>
+                            {Featureddata?.map((product) => (
+                                <div key={product.id} className='px-2'>
+                                    <ProductCard
+                                        image={product?.imges}
+                                        pCatagori={product?.pCatagori}
+                                        pName={product?.pName}
+                                        rate={product?.rate}
+                                        totalrating={product?.totalrating}
+                                        price={product?.price}
+                                        pdiscout={product?.discout}
+                                    />
+                                </div>
+                            ))}
+                        </Slider>
+                    </div>
                 </div>
-            </Container >
+            </Container>
         </>
-    )
-}
+    );
+};
 
-export default FeaturedProducts
+export default FeaturedProducts;
